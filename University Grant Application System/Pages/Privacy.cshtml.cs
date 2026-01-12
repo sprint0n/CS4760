e79e9ca69e3de3f52dba6a76425ac5e482396f8b
LoganBaty <143443942+LoganBaty@users.noreply.github.com>
2026-01-12 11:06:59 -0700

    Added Initial Files

---
+++ University Grant Application System/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace University_Grant_Application_System.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
