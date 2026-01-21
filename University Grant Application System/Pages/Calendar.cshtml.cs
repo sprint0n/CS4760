7c9388b953e6580f92cbd5b06a505ba30ad3b038
PramishM <ppmehta@mun.ca>
2026-01-20 21:40:41 -0700

    Added calendar feature

---
+++ University Grant Application System/Pages/Calendar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace University_Grant_Application_System.Pages
+{
+    public class CalendarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
